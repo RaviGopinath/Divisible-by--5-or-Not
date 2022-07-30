cd9b315066555c1c146432b754ab176f072f5193
RaviGopinath <gopinathravi95@gmail.com>
2022-07-30 11:56:36 +0530

    g

---
+++ divisible.js
@@ -0,0 +1,10 @@
+let n;
+
+n = parseInt(prompt("Please Enter the Number"));
+
+if(n % 5 == 0){
+    alert("Given number is divisible by 5")
+}
+else{
+    alert("Given number is not divisible by 5 ")
+}
